6b6e97ea7015309474f3796a37bb7065301cb82d
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-22 21:34:37 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
