cac30ce487be935c4cca0842402423a77198231a
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-23 12:52:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
+
+const API-KEY = "40845730-59b552d3cf1577a71be805545";
+
+const URL = "https://pixabay.com/api/"
