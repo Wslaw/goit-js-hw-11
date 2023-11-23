dae480f0dbeb2fc93b73b00f0a6c14b1b7d491dd
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-23 12:52:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,3 +8,12 @@ import axios from 'axios';
 const API-KEY = "40845730-59b552d3cf1577a71be805545";
 
 const URL = "https://pixabay.com/api/"
+
+// var API_KEY = '40845730-59b552d3cf1577a71be805545';
+// var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+// $.getJSON(URL, function(data){
+// if (parseInt(data.totalHits) > 0)
+//     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+// else
+//     console.log('No hits');
+// });
