9c2d7625bd33ff5999439a7218c9a21b85d45abd
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-24 23:08:51 +0200

    ALL

--- src/partials/back.js
+++ src/partials/back.js
@@ -9,3 +9,24 @@ backButton.addEventListener('click', () => {
   // Вернуться к началу страницы
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+// document.addEventListener('DOMContentLoaded', function () {
+//   const backToTopBtn = document.getElementById('backToTopBtn');
+
+//   // Показываем/скрываем кнопку при прокрутке
+//   window.addEventListener('scroll', function () {
+//     if (
+//       document.body.scrollTop > 20 ||
+//       document.documentElement.scrollTop > 20
+//     ) {
+//       backToTopBtn.style.display = 'block';
+//     } else {
+//       backToTopBtn.style.display = 'none';
+//     }
+//   });
+
+//   // Прокрутка вверх при клике на кнопку
+//   backToTopBtn.addEventListener('click', function () {
+//     document.body.scrollTop = 0; // Для Safari
+//     document.documentElement.scrollTop = 0; // Для других браузеров
+//   });
+// });
