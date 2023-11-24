cede1d34678bbd497edbed81c9394521a892e4d4
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-24 20:12:34 +0000

    Deploying to gh-pages from @ Wslaw/goit-js-hw-11@12c819f7e00812ab547325b5d2de0ed3682ce0fb 🚀

---
+++ index.d683f021.js
@@ -0,0 +1,2 @@
+var backButton=document.createElement("button");backButton.textContent="Back to top",backButton.classList.add("back-button"),document.body.appendChild(backButton),backButton.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.d683f021.js.map
