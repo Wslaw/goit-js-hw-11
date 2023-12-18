66bac6f1287362a8b4f3761e550fc33a9baa6d8d
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-12-18 15:42:18 +0200

    updateStars

--- src/index.js
+++ src/index.js
@@ -135,3 +135,17 @@ const createImageCard = ({ webformatURL, largeImageURL, tags, likes, views, comm
 //     lightbox.show();
 //     lightbox.refresh();
 // }
+// ************************************************
+const numStars = 900;
+
+function createStars() {
+  for (let i = 0; i < numStars; i++) {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.left = `${Math.random() * window.innerWidth}px`;
+    star.style.top = `${Math.random() * window.innerHeight}px`;
+    document.body.appendChild(star);
+  }
+}
+
+createStars();
